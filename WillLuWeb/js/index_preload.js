5f87312265f20d715b36e5f3a23746583a4efd65
X <77029666+William-f-12@users.noreply.github.com>
2024-09-27 11:02:05 -0500

    fix some problem with preload

--- WillLuWeb/js/index_preload.js
+++ WillLuWeb/js/index_preload.js
@@ -1,4 +1,7 @@
 // preload navbar
 $.get('../partials/_navbar.html', function (data) {
+    if (localStorage.getItem('navbar') && localStorage.getItem('navbar') !== data) {
+        localStorage.removeItem('navbar');
+    }
     localStorage.setItem('navbar', data);
 });
--- WillLuWeb/js/load_navbar.js
+++ WillLuWeb/js/load_navbar.js
@@ -1,11 +1,14 @@
 function loadNavbar() {
     let cachedNavbar = localStorage.getItem('navbar');
-    if (!cachedNavbar) {
+    if (cachedNavbar) {
+        $('#navbarContainer').html(cachedNavbar);
+    } else {
         $.get('../partials/_navbar.html', function (data) {
             localStorage.setItem('navbar', data);
+            $('#navbarContainer').html(data);
         });
     }
-    $('#navbarContainer').html(cachedNavbar);
+
 }
 
 document.addEventListener('DOMContentLoaded', loadNavbar);
