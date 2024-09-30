4ed2c831f9b30127d4d857581fec7f53a003f5c3
William-f-12 <3326007521@qq.com>
2024-09-30 13:09:35 -0500

    adjust the preload navbar, so that we won't use local storage

--- WillLuWeb/js/load_navbar.js
+++ WillLuWeb/js/load_navbar.js
@@ -1,14 +1,12 @@
 function loadNavbar() {
-    let cachedNavbar = localStorage.getItem('navbar');
-    if (cachedNavbar) {
-        $('#navbarContainer').html(cachedNavbar);
-    } else {
+    // let cachedNavbar = localStorage.getItem('navbar');
+    // if (cachedNavbar) {
+    //     $('#navbarContainer').html(cachedNavbar);
+    // } else {
         $.get('../partials/_navbar.html', function (data) {
-            localStorage.setItem('navbar', data);
             $('#navbarContainer').html(data);
         });
-    }
-
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', loadNavbar);
