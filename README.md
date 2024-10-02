52531d56f5205ea049a38b81693a69700fd10ea3
X <77029666+William-f-12@users.noreply.github.com>
2024-10-02 10:59:31 -0500

    change the implementation of navbar, update README.md

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # William-f-12.github.io
-William Lu's personal website
-Still in developing
+*William Lu's personal website*  
+*Still in developing*  
+
+# <a name="English"></a>Dev Log:  
+
+[Chinese Ver (中文版)](#Chinese)  
+
+*English Ver:*  
+## **First Phase:**  
+1. Create this repo, and start this project
+2. Create and adjust the basic structure of this project, including the directory hierarchy, not using jekyll, etc.  
+3. Build navigation bar with visual effect, deployed with JavaScript's dynamic loading, preloading to local storage and one single html/css file(s) that are used for all pages to reduces duplication and facilitates maintenance.  
+
+## **Second Phase: <- Current Phase**
+1. Update the README.md file to record dev log.
+2. Abandon the implementation of using the same navigation bar html/css file to improve performance.
+
+<br/><br/><br/><br/><br/>
+
+---
+
+<br/><br/><br/><br/><br/>
+
+# <a name="Chinese"></a>开发日志:    
+
+[英文版（English Ver）](#English)   
+
+*中文版:*  
+## **第一阶段：**  
+1. 创建此 repo 并启动此项目
+2. 创建并调整项目的基本结构，包括目录层次结构、不使用 jekyll 等。 
+3. 创建具有视觉效果的导航栏，使用 JavaScript 的动态加载，预加载到 local storage 进行部署，所有页面都使用同一个的 html/css 文件，以减少重复并方便维护。 
+
+## **第二阶段：<- 目前阶段**
+1. 更新 README.md 文件，记录开发日志。
+2. 舍弃掉用同一个导航栏 html/css 文件的实现，提升性能。
---
+++ WillLuWeb/js/navbar_effect.js
@@ -0,0 +1,53 @@
+// the navbar effect JavaScript file
+let marker = document.querySelector("#navBarMarker");
+let list = document.querySelectorAll('#navLinkList li');
+let navbar = document.querySelector("#navbar");
+let pageLink = document.querySelector("#curPage");
+
+// first move the marker to the right position according to the current page
+for (let i = 0; i < list.length; i++) {
+    if (list[i].classList.contains("active")) {
+        marker.style.left = 20 + 72 * i + 'px';
+        break;
+    }
+}
+
+// add active class in hovered list item
+function activeLink() {
+    list.forEach(item =>
+        item.classList.remove("active"));
+    this.classList.add("active");
+}
+
+list.forEach((item) =>
+    item.addEventListener("mouseover", activeLink));
+
+// move the marker to the active link
+function moveIndicator(e) {
+    marker.style.left = e.offsetLeft + 'px';
+}
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class' && mutation.target.className === 'active') {
+            // console.log('Class changed to:', mutation.target.className);
+            moveIndicator(mutation.target);
+        }
+    });
+});
+
+list.forEach((item) => {
+    observer.observe(item, {
+        attributes: true,
+        attributeFilter: ['class']
+    });
+})
+
+// move the indicator back to the right position according to the current page
+function moveIndicatorBack() {
+    list.forEach(item =>
+        item.classList.remove("active"));
+    pageLink.classList.add("active");
+}
+
+navbar.addEventListener("mouseout", moveIndicatorBack);
